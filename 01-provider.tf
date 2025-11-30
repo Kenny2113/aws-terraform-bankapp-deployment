5a62552ec2ca6648152e5ffe569d681b607e5373
Kennedy <kenedynwachukwu@gmail.com>
2025-11-30 14:37:37 +0100

    complete code

--- 01-provider.tf
+++ 01-provider.tf
@@ -1,6 +1,6 @@
-# provider "aws" {
-#   region = "us-east-1"
-# }
+provider "aws" {
+  region = "us-east-1"
+}
 
 
 terraform {
@@ -23,17 +23,3 @@ provider "namecheap" {
   client_ip = var.namecheap_client_ip
   use_sandbox = false
 }
-
-terraform {
-  required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.11.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.20.0"
-    }
-  }
-}
-
