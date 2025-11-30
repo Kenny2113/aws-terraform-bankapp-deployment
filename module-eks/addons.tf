ab963503b65a29b94a70d4cf4ef8928f01809d4f
Kennedy <kenedynwachukwu@gmail.com>
2025-11-30 09:51:32 +0100

    complete code

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -1,10 +1,11 @@
-provider "kubernetes" {
-  alias                  = "primary"
-  host                   = aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
+###############################
+# EKS Auth for Helm/Kubernetes
+###############################
+data "aws_eks_cluster_auth" "eks" {
+  name = aws_eks_cluster.eks.name
 }
 
+# Optional: only keep this if you actually use kubernetes_* resources elsewhere
 provider "kubernetes" {
   alias                  = "eks"
   host                   = aws_eks_cluster.eks.endpoint
@@ -12,22 +13,50 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.eks.token
 }
 
-
-data "aws_eks_cluster_auth" "eks" {
-    name = aws_eks_cluster.eks.name
+###############################
+# Helm Provider (REQUIRED)
+###############################
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
 }
+
+###############################
+# Wait until EKS is ACTIVE
+###############################
+resource "null_resource" "wait_for_eks" {
+  depends_on = [aws_eks_cluster.eks]
+
+  provisioner "local-exec" {
+    command = "aws eks wait cluster-active --name ${aws_eks_cluster.eks.name} --region ${var.region}"
+  }
+}
+
+###############################
+# NGINX INGRESS
+###############################
 resource "helm_release" "nginx_ingress" {
-    name       = "nginx-ingress"
-    repository = "https://kubernetes.github.io/ingress-nginx"
-    chart      = "ingress-nginx"
-    version    = "4.12.0"
-    namespace  = "ingress-nginx"
-    create_namespace = true
+  name             = "nginx-ingress"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  version          = "4.12.0"
+  namespace        = "ingress-nginx"
+  create_namespace = true
 
-    values = [file("${path.module}/nginx-ingress-values.yaml")]
-    depends_on = [ aws_eks_node_group.eks_node_group ]
+  values = [file("${path.module}/nginx-ingress-values.yaml")]
+
+  depends_on = [
+    null_resource.wait_for_eks,
+    aws_eks_node_group.eks_node_group
+  ]
 }
 
+###############################
+# Load Balancer Lookup (optional)
+###############################
 data "aws_lb" "nginx_ingress" {
   tags = {
     "kubernetes.io/service-name" = "ingress-nginx/nginx-ingress-ingress-nginx-controller"
@@ -36,6 +65,9 @@ data "aws_lb" "nginx_ingress" {
   depends_on = [helm_release.nginx_ingress]
 }
 
+###############################
+# CERT-MANAGER
+###############################
 resource "helm_release" "cert_manager" {
   name             = "cert-manager"
   repository       = "https://charts.jetstack.io"
@@ -56,14 +88,21 @@ resource "helm_release" "cert_manager" {
   ]
 }
 
-#==================================================
+###############################
+# ARGO CD
+###############################
 resource "helm_release" "argocd" {
-    name             = "argocd"
-    repository       = "https://argoproj.github.io/argo-helm"
-    chart            = "argo-cd"
-    version          = "5.51.6"
-    namespace        = "argocd"
-    create_namespace = true
-    values = [file("${path.module}/argocd-values.yaml")]
-    depends_on = [ helm_release.nginx_ingress, helm_release.cert_manager]
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = "5.51.6"
+  namespace        = "argocd"
+  create_namespace = true
+
+  values = [file("${path.module}/argocd-values.yaml")]
+
+  depends_on = [
+    helm_release.nginx_ingress,
+    helm_release.cert_manager
+  ]
 }
